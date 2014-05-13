2886e7d9b22006e5a55285585e12b932f2e8f360
Greg McCabe <gsmccabe1@gmail.com>
2014-05-13 15:11:18 -0500

    Construct a loop that iterates through each value and outputs only values with a type that is scalar.

--- foreach.php
+++ foreach.php
@@ -3,24 +3,29 @@
 	$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 	
 foreach($things as $thing){
-	
-	if (is_array($thing)) {
-		echo "{$thing} is an array\n";
-	}
-	elseif (is_bool($thing)) {
-		echo "{$thing} is a boolean\n";
-	}
-	elseif (is_float($thing)) {
-		echo "{$thing} is a float\n";
-	}
-	elseif (is_int($thing)) {
-		echo "{$thing} is an interger\n";
-	}
-	elseif (is_null($thing)) {
-		echo "{$thing} is NULL\n";
-	} else {
-		 echo "{$ting} is a string\n";
+	if (is_scalar($thing)) {
+		echo "{$thing} is a scalar\n";
 	}
 
 }
+
+
+	// if (is_array($thing)) {
+// 		echo "{$thing} is an array\n";
+// 	}
+// 	elseif (is_bool($thing)) {
+// 		echo "{$thing} is a boolean\n";
+// 	}
+// 	elseif (is_float($thing)) {
+// 		echo "{$thing} is a float\n";
+// 	}
+// 	elseif (is_int($thing)) {
+// 		echo "{$thing} is an interger\n";
+// 	}
+// 	elseif (is_null($thing)) {
+// 		echo "{$thing} is NULL\n";
+// 	} else {
+// 		 echo "{$ting} is a string\n";
+// 	}
+
 ?>
