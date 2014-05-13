959f5410d613aec1800a82e1495e38cd8ae138ca
Greg McCabe <gsmccabe1@gmail.com>
2014-05-13 17:49:15 -0500

    Create a file named foreach_books.php in you exercises directory. Commit each step and push to GitHub.
    Construct a loop that iterates through each book and then each book's keys and values. Have it output the book's title, then list the key value pairs for the data about each book.
    
    Update your loop to only show books that were written after 1950.

---
+++ foreach_books.php
@@ -0,0 +1,44 @@
+<?php
+	
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+// Construct a loop that iterates through each book and then each book's keys and values. Have it output the book's title, then list the key value pairs for the data about each book.
+
+
+foreach ($books as $titles => $details) {
+	if ($details['published'] >= 1950) {
+		echo "$titles\n";    
+		foreach ($details as $attributes => $value){	    
+			echo "$attributes\n";
+			echo "$value\n";
+		}	
+	}
+}			
+			
+?>				
+
+	
+
+	
+
+
