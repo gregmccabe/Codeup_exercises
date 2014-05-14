5020fae5275f5078c00613b5534eff5b6eee4a3e
Greg McCabe <gsmccabe1@gmail.com>
2014-05-14 11:28:03 -0500

    for loop with break

--- break_continue.php
+++ break_continue.php
@@ -1,7 +1,14 @@
 <?php
-for ($i = 0; $i < 102; $i++) { 
-	if ($i % 2 == 0) {
-		echo "$i\n";
+// for ($i = 0; $i < 102; $i++) { 
+// 	if ($i % 2 == 0) {
+// 		echo "$i\n";
+// 		continue;
+// 	}
+// }
+
+for ($i=0; $i < 102; $i++) { 
+	echo $i . "\n";
+	if ($i == 10) {
 		break;
 	}
 }
