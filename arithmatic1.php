a8b05b90ba415ef3a8650ef923a9dbfd490a6b6a
Greg McCabe <gsmccabe1@gmail.com>
2014-05-15 13:44:37 -0500

    arithmitic1 int

---
+++ arithmatic1.php
@@ -0,0 +1,11 @@
+Open the file arithmetic.php from your exercise repo for editing.
+
+Commit your changes for each step and push to GitHub.
+
+Validate all the arguments, and display an error if the input is not numeric.
+
+Validate divide by 0 errors, display error if attempts to divide by 0 are made.
+
+Make the error messages show the values of the arguments.
+
+Refactor the error messages into their own function, have the other functions use it for error messaging.
