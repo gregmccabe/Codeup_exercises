9120dcaf9f6cec4596aa9c43d7dd38f6977f6a7b
Greg McCabe <gsmccabe1@gmail.com>
2014-05-15 16:10:50 -0500

    arithmatic using return

--- arithmatic.php
+++ arithmatic.php
@@ -1,37 +1,37 @@
-
 <?php
 
 function add($a, $b) 
 {
 	if (is_numeric($a) && is_numeric($b)) 
 	{
-		echo $a + $b . PHP_EOL;
+		return $a + $b . PHP_EOL;
 	} else {
 		error();
 	}
 }
-add('10',10);
+echo add(10,10);
 
 function subtract($a, $b) 
 {
 	if (is_numeric($a) && is_numeric($b))
 	{
-		echo $a - $b . PHP_EOL;
+		return $a - $b . PHP_EOL;
 	} else {
 		error();
 	}
 }
-subtract(75,42);
+echo subtract(75,42);
+
 
 function multiply($a, $b) 
 {
 	if (is_numeric($a) && is_numeric($b)) {
-		echo $a * $b . PHP_EOL;
+		return $a * $b . PHP_EOL;
 	} else {
 		error();
 	}
 }
-multiply(30,3);
+echo multiply(30,3);
 
 function divide($a, $b) 
 {
@@ -39,24 +39,24 @@ function divide($a, $b)
 		if ($b == 0) {
 			echo "ERROR: can't divide by 0 yo!!\n";
 		} else {
-			echo $a / $b . PHP_EOL;
+			return $a / $b . PHP_EOL;
 		}
 	} else {
 		error();
 	}
 }
-divide(12,2);
+echo divide(12,2);
 
 function modulus($a, $b)
 {
 	if (is_numeric($a) && is_numeric($b)) 	
 	{
-		echo $a % $b . PHP_EOL;
+		return $a % $b . PHP_EOL;
 	} else {
 		error();
 	}
 }
-modulus(23,4);
+echo modulus(23,4);
 function error()
 {
 	echo "ERROR: Both arguments must be numbers\n";
