f089f8d0cdb395e9d6f8678db3ec36bab938fa23
Greg McCabe <gsmccabe1@gmail.com>
2014-05-20 14:19:59 -0500

    list_with_commas functions

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,15 +1,24 @@
 <?php
 
-$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-$physicists_array = explode(', ', $physicists_string);
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark'; //strin
+// $physicists_array = explode(', ', $physicists_string); //makes string to array
 
-$last_name = array_pop($physicists_array);  //remove last name
+// $last_name = array_pop($physicists_array);  //remove last name
 
-$physicists_string = implode(', ', $physicists_array) . ' and ' . $last_name;
+// $physicists_string = implode(', ', $physicists_array) . ' and ' . $last_name; //truns array to string with concatination
 
-// var_dump($physicists_string);
+// // var_dump($physicists_string);
 
-$famous_fake_physicists = $physicists_string;
+
+
+function humanized_list($physicists_string) {
+	$physicists_array = explode(', ', $physicists_string);
+	$last_name = array_pop($physicists_array);
+	$physicists_string = implode(', ', $physicists_array) . ' and ' . $last_name;
+	return $physicists_string;
+}
+
+$famous_fake_physicists = humanized_list($physicists_string);
 
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
