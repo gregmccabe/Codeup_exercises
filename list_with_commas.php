e422c5abd8c354f14eb57056e7a3f9f52fddb68f
Greg McCabe <gsmccabe1@gmail.com>
2014-05-20 14:56:01 -0500

    list_w_commas alpha sort w/default

--- list_with_commas.php
+++ list_with_commas.php
@@ -11,14 +11,22 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 
 
 
-function humanized_list($physicists_string) {
+function humanized_list($physicists_string, $alphaSort = false) {
+
 	$physicists_array = explode(', ', $physicists_string);
+
+
+	if ($alphaSort === true) {
+		sort($physicists_array);
+	}
+
 	$last_name = array_pop($physicists_array);
 	$physicists_string = implode(', ', $physicists_array) . ' and ' . $last_name;
+
 	return $physicists_string;
 }
 
-$famous_fake_physicists = humanized_list($physicists_string);
+$famous_fake_physicists = humanized_list($physicists_string, true);
 
 
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
